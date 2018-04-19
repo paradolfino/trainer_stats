ef02bb30370c3de29b4800914c2e84ae4291f3f8
Viktharien <viktharien@zoho.com>
2018-04-19 16:44:46 +0000

    create logs model, reference trainings with log, fix routes to have nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+=begin 
   root 'trainings#new'
   post '/' => 'trainings#create'
   get '/trainings' => 'trainings#index'
   get '/trainings/:id' => 'trainings#edit', as: 'training'
   post '/trainings/:id' => "trainings#update"
   resources :trainings
+=end
 end
