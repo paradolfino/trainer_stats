bb58898bb076c880ac82f1361f46ac0d9c0e0f65
Viktharien <viktharien@zoho.com>
2018-04-19 19:28:51 +0000

    set up 404

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -56,7 +56,7 @@ class LogsController < ApplicationController
     private
     
         def set_log
-           @log = Log.find(params[:id]) 
+           @log = Log.find(params[:id])  if @log
         end
         
         def params_log
