886d2d467efa5fc2c3efe397200c396d0c286eb9
Viktharien <viktharien@zoho.com>
2018-04-19 20:44:23 +0000

    start work on styling views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     include Response
     def not_found
-        render '#{Rails.root}/public/404.html}'
+        redirect_to '/public/404.html'
     end
 end
