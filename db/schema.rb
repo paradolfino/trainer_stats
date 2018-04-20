2212ffa8182a1a1ca2ec8644a3ddbb9d6858a40c
Viktharien <viktharien@zoho.com>
2018-04-20 13:59:11 +0000

    minor styling changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_19_164215) do
+ActiveRecord::Schema.define(version: 2018_04_18_004216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "logs", force: :cascade do |t|
-    t.string "title"
-    t.boolean "active"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "trainings", force: :cascade do |t|
     t.string "member"
