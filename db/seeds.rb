05400a8f2461ddc91091987834386d233a4e2001
Viktharien <viktharien@zoho.com>
2018-04-20 18:04:24 +0000

    fix css to auto scroll table content

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do 
+   Log.create(title: "Test", active: true) 
+end
