a8c15820dc5327000b4bc2e9887af194485cfb5a
Viktharien <viktharien@zoho.com>
2018-04-22 23:06:57 +0000

    implement csv export with logs show action

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -33,8 +33,8 @@ class LogsController < ApplicationController
             format.json { 
                     json_response(@log.to_json(:include => [:trainings]))  
             }
-            format.csv { send_data @trainings.to_csv, filename "#{@log.title}.csv" }
-            format.xls { send_data @trainings.to_csv(col_sep: "\t"), filename "#{@log.title}.xls" }
+            format.csv { send_data @trainings.to_csv, filename: "#{@log.title}.csv" }
+            format.xls { send_data @trainings.to_csv(col_sep: "\t"), filename: "#{@log.title}.xls" }
         end
         
         
