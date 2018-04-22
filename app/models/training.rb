b45bd62d51b1bc559dc2badc37cd162c72ee191a
Viktharien <viktharien@zoho.com>
2018-04-22 22:47:49 +0000

    implement csv export with logs show action

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,3 +1,11 @@
 class Training < ApplicationRecord
     belongs_to :log
+    def self.to_csv(options = {})
+      CSV.generate(options) do |csv|
+        csv << column_names
+        all.each do |training|
+          csv << training.attributes.values_at(*column_names)
+        end
+      end
+    end
 end
