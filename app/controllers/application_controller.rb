a57422bf8055cb06400222f6c7830ed71e4541e3
Viktharien <viktharien@zoho.com>
2018-05-08 16:33:22 +0000

    integrate devise, toastr, and authorization features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,29 @@
 class ApplicationController < ActionController::Base
     include Response
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    helper_method :is_admin?
+    
     def not_found
         redirect_to '/public/404.html'
+    end
+    
+    def is_admin?
+        return true if current_user && current_user.role == "Admin"
+        false
+    end
+    
+    def require_admin
+        if !is_admin?
+           flash[:alert] = "You must be an admin to view this!"
+           redirect_to '/'
+        end
+    end
+
+    
+    protected
+    
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:edit, keys: [:name])
     end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :destroy]
+    before_action :authenticate_user!
+    before_action :require_admin, only: [:new, :index, :destroy]
+    
+    def index
+        @users = User.all
+    end
+    
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to '/'
+        end
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        if params[:user][:password].blank?
+          params[:user].delete(:password)
+          params[:user].delete(:password_confirmation)
+        end
+        if @user.update(user_params)
+           flash[:notice] = "User #{@user.name} has been updated!" 
+           redirect_to '/admin/users'
+        else
+            flash[:alert] = "There were errors when attempting to update, please review before submitting."
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        flash[:notice] = "#{@user.name} has been destroyed."
+        @user.destroy
+        redirect_to '/admin/users'
+    end
+    
+    private
+    
+        def set_user
+           @user = User.find(params[:id]) 
+        end
+    
+        def user_params
+           params.require(:user).permit(:name, :email, :password, :password_confirmation, :role) 
+        end
+end
