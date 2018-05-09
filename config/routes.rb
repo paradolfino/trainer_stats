69d0276cfc7f9a544ad9ee7b88f03f0dcd771e9c
Viktharien <viktharien@zoho.com>
2018-05-09 15:32:24 +0000

    add events routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   end
   scope '/admin' do
     resources :users
+    get 'audit' => 'events#audit'
+    resources :events, only: :create
   end
   
   get '/logs/inactive' => 'logs#inactive', as: 'inactive_logs'
