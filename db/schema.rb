6516e8c60508ee8375261b7e68a242c46753ac38
Viktharien <viktharien@zoho.com>
2018-05-09 15:17:11 +0000

    implement audit trail with event model and events controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_08_154628) do
+ActiveRecord::Schema.define(version: 2018_05_09_151622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "action"
+    t.string "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "logs", force: :cascade do |t|
     t.string "title"
@@ -61,4 +70,5 @@ ActiveRecord::Schema.define(version: 2018_05_08_154628) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
