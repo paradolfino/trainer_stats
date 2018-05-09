f4d0053562539da53f35232dd075223fc3481972
Viktharien <viktharien@zoho.com>
2018-05-09 15:18:52 +0000

    implement D2 changes to controllers including methods for authorization in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,31 @@ class ApplicationController < ActionController::Base
            redirect_to '/'
         end
     end
+    
+    def require_same_user(user)
+        if is_admin?
+            if current_user.id != user.id && user.role == "Admin"
+                flash[:alert] = "You can't modify or delete other admins!"
+                redirect_to '/'
+            end
+        else    
+            if current_user.id != user.id
+                flash[:alert] = "Something went wrong!"
+                redirect_to '/'
+            end
+        end
+        
+    end
+    
+    def create_event(action, content)
+        @event = Event.create(action: action, content: content, user_id: current_user.id)
+    end
 
     
     protected
     
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-        devise_parameter_sanitizer.permit(:edit, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+        devise_parameter_sanitizer.permit(:edit, keys: [:fullname])
     end
 end
